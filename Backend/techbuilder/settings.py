8aa2ecab9284c5565b5dbd916b38d9e4f5705da4
Anindoakb <anindo.biswas.232@northsouth.edu>
2025-10-03 22:44:56 +0600

    feat: enable WhiteNoise for static files (middleware + storages)

--- Backend/techbuilder/settings.py
+++ Backend/techbuilder/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,6 +146,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'productionfiles'
+
+# WhiteNoise static files storage (Django 5+ STORAGES setting)
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    }
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
